3ba0233c30163791718cab0be7d383506cdbef28
dhivyasundaram <107605296+dhivyasundaram@users.noreply.github.com>
2022-06-24 21:28:22 +0530

    Add files via upload

---
+++ CharOccurance.java
@@ -0,0 +1,23 @@
+package week2day1;
+
+public class CharOccurance {
+public static void main(String[] args) {
+	String str = "welcome to chennai";
+	int count = 0;
+	for(int i=0; i< str.length();i++) {
+              if(str.charAt(i)=='e')
+              count++;
+	}
+              System.out.println(count);
+              
+			
+		
+			
+		
+	}
+	
+}
+	
+	
+
+
---
+++ ReverseString.java
@@ -0,0 +1,19 @@
+package week2day1;
+
+public class ReverseString {
+	public static void main(String[] args) {
+		String rev = "feeling good";
+	char[] ch = rev.toCharArray();
+			for (int i= rev.length()-1;i>=0;i--) {
+System.out.println(ch[i]);
+
+				
+				
+			}
+				
+		}
+		
+		
+	}
+
+
